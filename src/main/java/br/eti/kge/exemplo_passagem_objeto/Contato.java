05f430b166dadc2a523bdc53214e6ae16165d81b
raelnascimento <nascimentoisrael19@gmail.com>
2022-04-26 13:57:28 -0300

    Adicionando exemplo de passagem de objeto  - v1.0

---
+++ src/main/java/br/eti/kge/exemplo_passagem_objeto/Contato.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.eti.kge.exemplo_passagem_objeto;
+
+/**
+ *
+ * @author KGe
+ */
+public class Contato {
+    public int id;
+    public String nome;
+    public String telefone;
+
+    public Contato() {
+        
+    }
+    
+    public Contato(int id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
